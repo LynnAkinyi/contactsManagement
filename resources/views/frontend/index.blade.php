a95b18348003612a90433977b060d3122e503a7d
LynnAkinyi <78434875+LynnAkinyi@users.noreply.github.com>
2024-06-13 14:24:26 +0300

    create table and model, makemigrations

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,5 +1,20 @@
 <x-app-web-layout>
-    <h2>This is the first step</h2>
-    <h2>This is the first step</h2>
-    <p>Moving on easy</p>
+
+    <x-slot:title>
+        Contacts Management App
+        </x-slot>
+        <div class="py-5">
+            <div class="container">
+                <h2>This is the first step</h2>
+                <h2>This is the first step</h2>
+                <p>Moving on easy</p>
+            </div>
+        </div>
+
+        <x-slot:scripts>
+            <script>
+                console.log('This is the script');
+                alert('Do you want to add a contact');
+            </script>
+            </x-slot>
 </x-app-web-layout>
